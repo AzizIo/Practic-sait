1a5c74f5735628ee307c85ea7adb3216aafc51a2
AzizIo <azizbekmadzitov@gmail.com>
2026-03-03 05:05:13 +0300

    Блок контакты получил новый дизайн. Логотип теперь как ссылка на главную

--- frontend/src/pages/ContactsPage.tsx
+++ frontend/src/pages/ContactsPage.tsx
@@ -1,36 +1,140 @@
-import Navigation from "../components/Navigation";
+import styles from './ContactsPage.module.css'
+import Navigation from '../components/Navigation'
 
 export default function ContactsPage() {
-  return (
-    <>
-      <Navigation />
-      <main style={{ padding: 24 }}>
-        <h1>Контактная информация</h1>
+    return (
 
-        <h2>Центр программирования и кибербезопасности</h2>
-        <p>Руководитель: Велибеков Казихан Исмаилович</p>
-        <p>Адрес: г. Москва, ул. Академика Миллионщикова, д. 20</p>
-        <p>Телефон: +7 (985) 332-08-39</p>
-        <p>E-mail: Velibekov_K@itmoscow.pro</p>
+        <>
+            <Navigation />
+            <section className={styles.contacts}>
+                <h1 className={styles.contacts_title}>Контактная информация</h1>
+                <div className={styles.contact_block}>
 
-        <h2>Digital.Колледж</h2>
-        <p>Руководитель: Емельянова Александра Сергеевна</p>
-        <p>Адрес: г. Москва, проспект Вернадского, дом 29А</p>
-        <p>Телефон: +7 (926) 815-35-34</p>
-        <p>E-mail: EmelyanovaAS@itmoscow.pro</p>
+                    <div className={styles.contact_card}>
+                    <h2 className={styles.contact_card__title}>
+                            Центр программирования<br/>и кибербезопасности
+                        </h2>
 
-        <h2>IT.Бирюлево</h2>
-        <p>Руководитель: Сугатова Анна Юрьевна</p>
-        <p>Адрес: г. Москва, проезд Харьковский, дом 5А</p>
-        <p>Телефон: +7 (985) 017-51-92</p>
-        <p>E-mail: sugatova_au@itmoscow.pro</p>
+                        <div className={styles.contact_item}>
+                            <span className={styles.label}>Руководитель</span>
+                            <span className={styles.value}>Велибеков Казихан Исмаилович</span>
+                        </div>
 
-        <h2>Центр городских технологий</h2>
-        <p>Руководитель: Терехов Евгений Васильевич</p>
-        <p>Адрес: г. Москва, улица Судостроительная, дом 48</p>
-        <p>Телефон: +7 (925) 759-60-92</p>
-        <p>E-mail: terekhov@itmoscow.pro</p>
-      </main>
-    </>
-  );
+                        <div className={styles.contact_item}>
+                            <span className={styles.label}>Адрес</span>
+                            <span className={styles.value}>
+                                г. Москва, ул. Академика Миллионщикова, д. 20
+                            </span>
+                        </div>
+
+                        <div className={styles.contact_item}>
+                            <span className={styles.label}>Телефон</span>
+                            <a href="tel:+79853320839" className={`${styles.value} ${styles.link}`}>
+                                +7 (985) 332-08-39
+                            </a>
+                        </div>
+
+                        <div className={styles.contact_item}>
+                            <span className={styles.label}>E-mail</span>
+                            <a href="mailto:Velibekov_K@itmoscow.pro" className={`${styles.value} ${styles.link}`}>
+                                Velibekov_K@itmoscow.pro
+                            </a>
+                        </div>
+                    </div>
+                    <div className={`${styles.contact_card} ${styles.contact_card2}`}>
+                        <h2 className={styles.contact_card__title}>
+                            Digital.Колледж
+                        </h2>
+
+                        <div className={styles.contact_item}>
+                            <span className={styles.label}>Руководитель</span>
+                            <span className={styles.value}>Емельянова Александра Сергеевна</span>
+                        </div>
+
+                        <div className={styles.contact_item}>
+                            <span className={styles.label}>Адрес</span>
+                            <span className={styles.value}>
+                                г. Москва, проспект Вернадского, дом 29А
+                            </span>
+                        </div>
+
+                        <div className={styles.contact_item}>
+                            <span className={styles.label}>Телефон</span>
+                            <a href="tel:+79853320839" className={`${styles.value} ${styles.link}`}>
+                                +7 (926) 815-35-34
+                            </a>
+                        </div>
+
+                        <div className={styles.contact_item}>
+                            <span className={styles.label}>E-mail</span>
+                            <a href="mailto:EmelyanovaAS@itmoscow.pro" className={`${styles.value} ${styles.link}`}>
+                                EmelyanovaAS@itmoscow.pro
+                            </a>
+                        </div>
+                    </div>
+                    <div className={`${styles.contact_card} ${styles.contact_card3}`}>
+                        <h2 className={styles.contact_card__title}>
+                            IT.Бирюлево
+                        </h2>
+
+                        <div className={styles.contact_item}>
+                            <span className={styles.label}>Руководитель</span>
+                            <span className={styles.value}>Сугатова Анна Юрьевна</span>
+                        </div>
+
+                        <div className={styles.contact_item}>
+                            <span className={styles.label}>Адрес</span>
+                            <span className={styles.value}>
+                                г. Москва, проезд Харьковский, дом 5А
+                            </span>
+                        </div>
+
+                        <div className={styles.contact_item}>
+                            <span className={styles.label}>Телефон</span>
+                            <a href="tel:+79853320839" className={`${styles.value} ${styles.link}`}>
+                                +7 (985) 017-51-92
+                            </a>
+                        </div>
+
+                        <div className={styles.contact_item}>
+                            <span className={styles.label}>E-mail</span>
+                            <a href="mailto:sugatova_au@itmoscow.pro" className={`${styles.value} ${styles.link}`}>
+                                sugatova_au@itmoscow.pro
+                            </a>
+                        </div>
+                    </div>
+                    <div className={`${styles.contact_card} ${styles.contact_card4}`}>
+                        <h2 className={styles.contact_card__title}>
+                            Центр городских технологий
+                        </h2>
+
+                        <div className={styles.contact_item}>
+                            <span className={styles.label}>Руководитель</span>
+                            <span className={styles.value}>Терехов Евгений Васильевич</span>
+                        </div>
+
+                        <div className={styles.contact_item}>
+                            <span className={styles.label}>Адрес</span>
+                            <span className={styles.value}>
+                                г. Москва, улица Судостроительная, дом 48
+                            </span>
+                        </div>
+
+                        <div className={styles.contact_item}>
+                            <span className={styles.label}>Телефон</span>
+                            <a href="tel:+79853320839" className={`${styles.value} ${styles.link}`}/>
+                                +7 (925) 759-60-92
+                        </div>
+
+                        <div className={styles.contact_item}>
+                            <span className={styles.label}>E-mail</span>
+                            <a href="mailto:terekhov@itmoscow.pro" className={`${styles.value} ${styles.link}`}>
+                                terekhov@itmoscow.pro
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
 }
